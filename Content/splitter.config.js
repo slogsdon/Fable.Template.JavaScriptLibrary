db608bfa07b9f3d7ca1b4ee77cbc33501cb1f6ae
Shane Logsdon <shane@shanelogsdon.com>
2017-12-08 08:19:10 -0500

    configure generated project for es5/commonjs

--- Content/splitter.config.js
+++ Content/splitter.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const fableUtils = require('fable-utils');
 
 const name = 'FableTemplate.JsLib';
 const resolve = (relativePath) => path.join(__dirname, relativePath);
 
 module.exports = {
+  name: name,
   entry: resolve(`./src/${name}.fsproj`),
   outDir: resolve(`./dist`),
-}
-module.exports.name = name;
+  babel: fableUtils.resolveBabelOptions({
+    presets: [['env', { modules: 'commonjs' }]],
+    sourceMaps: true,
+  }),
+};
